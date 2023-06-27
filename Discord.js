3214003d069a3dfeda61c09f977dd0244766f09c
Áom | Siriwat J <siriwatjaiyungyuen@gmail.com>
2023-06-27 21:38:04 +0700

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -2,8 +2,6 @@ function joinAndLeave() {
     let startCall = document.querySelector('[aria-label="Start Voice Call"]');
     let endCall = document.querySelector('[aria-label="Disconnect"]');
     let joinCall = document.querySelector('[aria-label="Join Call"]')
-    console.log(startCall);
-    console.log(endCall);
     if (startCall == null && endCall == null) {
         joinCall.click();
     } else if (startCall == null) {
@@ -11,6 +9,10 @@ function joinAndLeave() {
     } else {
         startCall.click();
     }
+    sleep(1500);
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let intV = setInterval(joinAndLeave, 1000);
+setInterval(joinAndLeave, 1000);
